8d7e3f8c1c6020aec413d6e046b8d3ac28f4e4e5
Avishka Shehan <shehanw@sliclife.com>
2026-06-11 11:32:28 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if target > matrix[-1][-1] or target < matrix[0][0]:
+            return False
+        
+        low = 0
+        high = len(matrix) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            valueRow = matrix[mid]
+
+            if valueRow[0] <= target and target <= valueRow[-1]:
+                break;
+            elif valueRow[0] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        innerLow = 0
+        innerHigh = len(valueRow) - 1    
+        while innerLow <= innerHigh:
+            innerMid = innerLow + (innerHigh - innerLow) // 2
+            guess = valueRow[innerMid]
+
+            if guess == target:
+                return True
+            elif guess > target:
+                innerHigh = innerMid - 1
+            else:
+                innerLow = innerMid + 1
+        return False
+
