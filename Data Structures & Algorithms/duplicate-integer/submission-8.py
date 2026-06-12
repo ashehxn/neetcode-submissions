61433c36356650532c6a489935665bc17c4f9789
Avishka Shehan <shehanw@sliclife.com>
2026-06-12 13:19:45 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,11 @@
+# Hash Set
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
